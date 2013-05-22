41b2d9cdb6ce8a924d73bb763d25e003f22d8c11
Niklas Semmler <nsemmler@net.t-labs.tu-berlin.de>
2013-05-22 22:43:47 +0200

    added first test functionality

---
+++ 3/mw3a/StockExchange/test/BareFunctionality.java
@@ -0,0 +1,43 @@
+package StockExchange.test;
+
+import StockExchange.QuoterImpl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.PrintStream;
+import java.io.OutputStream;
+
+public class BareFunctionality {
+    private static QuoterImpl quote;
+
+    public BareFunctionality() {
+    }
+    @BeforeClass
+    public static void testSetup() {
+        PrintStream originalStream = System.out;
+        PrintStream dummyStream    = new PrintStream(new OutputStream(){
+            public void write(int b) {
+                //NO-OP
+            }
+        });
+        System.setOut(dummyStream);
+        quote = new QuoterImpl();
+        System.setOut(originalStream);
+    }
+    @Test
+    public void test_quote_by_name() {
+        float ret = quote.getQuoteByName("adidas AG");
+        assertEquals("could not get quote by name", 85.52, ret, 0.01);
+    } 
+    @Test
+    public void test_quote_by_id() {
+        float ret = quote.getQuoteByID("DE000A1EWWW0");
+        assertEquals("could not get quote by ID", 85.52, ret, 0.01);
+    }
+}
---
+++ 3/mw3a/StockExchange/test/Run.java
@@ -0,0 +1,14 @@
+package StockExchange.test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class Run {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(BareFunctionality.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+    }
+} 
