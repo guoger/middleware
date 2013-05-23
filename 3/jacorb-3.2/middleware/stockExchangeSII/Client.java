62664f38e8bcdd81d0263c07753720fe8edadee0
Niklas Semmler <nsemmler@net.t-labs.tu-berlin.de>
2013-05-23 21:27:16 +0200

    added revised Client

--- 3/jacorb-3.2/middleware/stockExchangeSII/Client.java
+++ 3/jacorb-3.2/middleware/stockExchangeSII/Client.java
@@ -11,6 +11,48 @@ public class Client {
 		// TODO Auto-generated constructor stub
 	}
 
+    public static void start( String[] args ) throws Exception {
+        File f = new File(args[0]);
+        // check if file exists
+        if(! f.exists()) {
+            System.out.println(" [CLIENT]\tFile " + args[0] + " does not exist.");
+            System.exit(-1);
+        }
+        // check if args[0] points to a directory
+        if( f.isDirectory() )
+        {
+            System.out.println(" [CLIENT]\tFile " + args[0] + " is a directory.");
+            System.exit( -1 );
+        }
+        
+        // get stock arg
+        for (int i=1; i<args.length; i++) {
+            req += (args[i] + " ");
+        }
+        req = req.substring(0, req.length() - 1);
+        
+        // init the ORB
+        ORB orb = ORB.init(args, null);
+        
+        // get object from IOR file
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        org.omg.CORBA.Object obj = orb.string_to_object(br.readLine());
+        
+        br.close();
+        
+        // narrow it to RequestQuote
+        RequestQuote reqQuote = RequestQuoteHelper.narrow(obj);
+        
+        // invoke request operation
+        double quote = reqQuote.reqQuote(req);
+        if( quote != -1 ) {
+            System.out.println(" [CLIENT]\t\tName/ID\t\t\tQuote");
+            System.out.println(" [CLIENT]\t\t"+req+"\t\t"+quote);
+        } else {
+            throw new Exception("[CLIENT]\t\tInvalid input!!");
+        }
+    }
+
 	public static void main(String[] args) {
 		if (args.length <= 1) {
 			System.out.println(" [CLIENT]\tIOR file and stock request txt required!");
@@ -18,45 +60,7 @@ public class Client {
 		}
 		
 		try {
-			File f = new File(args[0]);
-			// check if file exists
-			if(! f.exists()) {
-				System.out.println(" [CLIENT]\tFile " + args[0] + " does not exist.");
-				System.exit(-1);
-			}
-			// check if args[0] points to a directory
-			if( f.isDirectory() )
-            {
-                System.out.println(" [CLIENT]\tFile " + args[0] + " is a directory.");
-                System.exit( -1 );
-            }
-			
-			// get stock arg
-			for (int i=1; i<args.length; i++) {
-				req += (args[i] + " ");
-			}
-			req = req.substring(0, req.length() - 1);
-			
-			// init the ORB
-			ORB orb = ORB.init(args, null);
-			
-			// get object from IOR file
-			BufferedReader br = new BufferedReader(new FileReader(f));
-			org.omg.CORBA.Object obj = orb.string_to_object(br.readLine());
-			
-			br.close();
-			
-			// narrow it to RequestQuote
-			RequestQuote reqQuote = RequestQuoteHelper.narrow(obj);
-			
-			// invoke request operation
-      double quote = reqQuote.reqQuote(req);
-      if( quote != -1 ) {
-        System.out.println(" [CLIENT]\t\tName/ID\t\t\tQuote");
-        System.out.println(" [CLIENT]\t\t"+req+"\t\t"+quote);
-      } else {
-        System.out.println(" [CLIENT]\t\tInvalid input!!");
-      }
+            Client.start(args);
 		} catch(Exception ex) {
 			System.out.println(ex);
 		}
