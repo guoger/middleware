1aacd023b651c6fdc48c72b064e338c383447a81
guoger <guogerger@gmail.com>
2013-06-28 03:57:01 +0200

    a demo client HelloWorld

---
+++ 5/src/demoRequest/HelloWorldDemo.java
@@ -0,0 +1,40 @@
+package demoRequest;
+
+import java.io.File;
+
+import util.ParamList;
+import util.Parameter;
+
+import client.Request;
+
+public class HelloWorldDemo extends Request {
+
+	public HelloWorldDemo(File file) {
+		super(file);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected ParamList[] formParams() {
+		// TODO Auto-generated method stub
+		ParamList pl = new ParamList("withPar");
+		Class<?> paramType = float.class;
+		Object paramVal = (float) 1.0;
+		Parameter parameter = new Parameter(paramType, paramVal);
+		pl.insert(parameter);
+		paramType = String.class;
+		paramVal = "OK";
+		parameter = new Parameter(paramType, paramVal);
+		pl.insert(parameter);
+		return new ParamList[]{pl};
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
