2ffcb1106881c82cd73b407b1fef8645af3efbe0
guoger <guogerger@gmail.com>
2013-06-30 00:17:18 +0200

    java file for test

---
+++ 5/clientpool/HelloWorld.java
@@ -0,0 +1,39 @@
+public class HelloWorld {
+
+	public HelloWorld() {
+
+	}
+
+	public static void foo() {
+		System.out.println("foo");
+	}
+
+	public static void bar() {
+		System.out.println("bar");
+		foo();
+	}
+
+	public static void withPar(Float a, String b) {
+		System.out.println("withPar(float " + a + ", String " + b + ")");
+	}
+
+	public static void withPar(String a, String b) {
+		System.out.println("withPar(String " + a + ", String " + b + ")");
+	}
+
+	public void withPar(float a, String b) {
+		System.out.println("withPar(float " + a + ", String " + b + ")");
+	}
+
+	public static float number() {
+		System.out.println("Hello world!");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 1;
+	}
+
+}
